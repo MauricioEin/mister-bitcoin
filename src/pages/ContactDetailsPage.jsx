510d2c1c54f1d6d44ca46c593fefc55ea7a0a9e3
MauricioEin <mauricio.ein@gmail.com>
2022-12-26 15:23:53 +0200

    prev next contact details

--- src/pages/ContactDetailsPage.jsx
+++ src/pages/ContactDetailsPage.jsx
@@ -1,24 +1,47 @@
 import { Component } from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 import { contactService } from '../services/contact.service'
 
 export class ContactDetailsPage extends Component {
     state = {
-        contact: null
+        contact: null,
+        prevId: null,
+        nextId: null
     }
 
     async componentDidMount() {
-        const contact = await contactService.getContactById(this.props.match.params.id)
-        this.setState({ contact })
+        this.loadContact()
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.match.params.id !== this.props.match.params.id) {
+            this.loadContact()
+        }
+    }
+
+
+    loadContact = async () => {
+        const id = this.props.match.params.id
+        const contact = await contactService.getContactById(id)
+        // for prev and next:
+        const contacts = await contactService.getContacts()
+        const idx = contacts.findIndex(contact => contact._id === id)
+        const nextId = contacts[(contacts.length + idx + 1) % contacts.length]._id
+        const prevId = contacts[(contacts.length + idx - 1) % contacts.length]._id
+        this.setState({ contact, nextId, prevId })
     }
 
     render() {
-        const { contact } = this.state
+        const { contact, nextId, prevId } = this.state
         if (!contact) return <div>Loading...</div>
         const imgUrl = `https://robohash.org/${contact._id}`
 
         return (
             <div className="contact-details">
+                <nav className='flex space-around'>
+                    <Link to={`/contact/${prevId}`}> Previous</Link>
+                    <Link to={`/contact/${nextId}`}> Next</Link>
+                </nav>
                 <img src={imgUrl} alt="" />
 
                 <h1>{contact.name}</h1>
